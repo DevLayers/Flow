7d6f0e8874d29df9880e7779a4adcad3812bc1c0
P3DROVFX <pedrolucasmunizpessoa@gmail.com>
2026-08-05 03:09:27 -0300

    fix(hyprland): workspace mouse scroll inverted in 0.56.1 and restored env.lua

--- dots/.config/hypr/hyprland/env.lua
+++ dots/.config/hypr/hyprland/env.lua
@@ -8,16 +8,7 @@ hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
 -- Applications
 local xdg_data_dirs_old = os.getenv("XDG_DATA_DIRS") or ""
-hl.env("XDG_DATA_DIRS",
-    home_dir ..
-    "/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share:" ..
-    xdg_data_dirs_old)
-
--- Cursor theme
-hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
-hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XDG_DATA_DIRS", home_dir .. "/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share:" .. xdg_data_dirs_old)
 
 -- Themes
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
